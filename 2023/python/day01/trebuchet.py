429c2279604497db51ea78e9dc820aa75986c44f
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-12-02 17:39:22 +0100

    Simplify code

--- 2023/python/day01/trebuchet.py
+++ 2023/python/day01/trebuchet.py
@@ -1,21 +1,17 @@
 """
 https://adventofcode.com/2023/day/1
 """
-from typing import List, Optional, Tuple
+from typing import List, Tuple
 
 
 def trebuchet(input: str):
-    calibration_values: List[int] = []
+    result = 0
     for line in input.splitlines():
         digits: List[str] = list(filter(lambda c: c.isdigit(), line))
 
-        fl: Tuple[Optional[int], Optional[int]] = (None, None)
+        if len(digits) > 0:
+            fl: Tuple[int, int] = (int(digits[0]), int(digits[-1]))
 
-        if len(digits) > 1:
-            fl = (int(digits[0]), int(digits[-1]))
-        if len(digits) == 1:
-            fl = (int(digits[0]), int(digits[0]))
+            result += int(f"{fl[0]}{fl[1]}")
 
-        if fl[0] is not None:
-            calibration_values.append(int(f"{fl[0]}{fl[1]}"))
-    return sum(calibration_values)
+    return result
