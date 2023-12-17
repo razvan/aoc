3168e5c505162557033b41f1324dfcab0aee52dd
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-12-17 21:36:23 +0100

    2023 day06 part 1

--- 2023/python/day06/__init__.py
+++ 2023/python/day06/__init__.py
@@ -1,10 +1,44 @@
+from dataclasses import dataclass, field
 import sys
+from parsy import eof, generate, regex, string, whitespace
+from typing import List, Tuple, Iterable
+from math import prod
+
+
+@dataclass(frozen=True, slots=True)
+class Race:
+    time: int
+    dist: int
+
+    def options(self) -> Iterable[Tuple[int, int]]:
+        return [(hold, hold * (self.time - hold)) for hold in range(self.time + 1)]
+
+    def count_winning(self) -> int:
+        return len([o for o in self.options() if o[1] > self.dist])
+
+
+@dataclass(frozen=True, slots=True)
+class Event:
+    races: List[Race] = field(default_factory=list)
+
+    def count_winning(self) -> int:
+        return prod([r.count_winning() for r in self.races])
+
+
+@generate
+def parser():
+    _num = regex(r"\d+").map(int)
+    _pad = regex(r"\s*")
+    times = yield _pad >> string("Time:") >> whitespace >> _num.sep_by(whitespace)
+    dist = yield _pad >> string("Distance:") >> whitespace >> _num.sep_by(whitespace)
+    yield _pad | eof
+    return Event([Race(t, d) for t, d in zip(times, dist)])
 
 
 def main(f: str):
     with open(f) as input:
         strin = input.read()
-        # print("Day 05: min location is {}".format(a.min_location()))
+        print("Day 06: part 1 is {}".format(parser.parse(strin).count_winning()))
 
 
 if __name__ == "__main__":
--- 2023/python/day06/test_part1.py
+++ 2023/python/day06/test_part1.py
@@ -1,1 +1,29 @@
+from unittest import TestCase
+from . import Event, Race, parser
 
+
+class Day06(TestCase):
+    input = """
+        Time:      7  15   30
+        Distance:  9  40  200"""
+
+    def test_day06_event_parse(self):
+        got = parser.parse(self.input)
+        expected = Event([Race(7, 9), Race(15, 40), Race(30, 200)])
+        self.assertEqual(expected, got)
+
+    def test_day06_race_moves(self):
+        ev = parser.parse(self.input)
+        got = ev.races[0].options()
+        expected = [(0, 0), (1, 6), (2, 10), (3, 12), (4, 12), (5, 10), (6, 6), (7, 0)]
+        self.assertEqual(expected, got)
+
+    def test_day06_count_winning(self):
+        ev = parser.parse(self.input)
+        got = ev.races[0].count_winning()
+        self.assertEqual(4, got)
+
+    def test_day06_event_count_winning(self):
+        ev = parser.parse(self.input)
+        got = ev.count_winning()
+        self.assertEqual(288, got)
