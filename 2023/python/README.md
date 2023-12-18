f8c5e8211f4893b61634b9636ed0abbd6768dc11
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-12-18 11:13:37 +0100

    2023 extract input from the python dir

--- 2023/python/README.md
+++ 2023/python/README.md
@@ -4,6 +4,6 @@
 
 1. Install [pixi](https://github.com/prefix-dev/pixi)
 2. Run all puzzles: `pixi run aoc`
-3. Run individual puzzles (example with day06): `pixi run python -m day06 input/day06/part*`
+3. Run individual puzzles (example with day06): `pixi run python -m day06 ../input/day06/part*`
 4. To run the tests `pixi run test`
 
--- 2023/python/aoc.py
+++ 2023/python/aoc.py
@@ -6,11 +6,11 @@ import day06
 
 
 def main():
-    day02.main("input/day02/input")
-    day03.main("input/day03/input")
-    day04.main("input/day04/input.txt")
-    day05.main("input/day05/input.txt")
-    day06.main(["input/day06/part1", "input/day06/part2"])
+    day02.main("../input/day02/input")
+    day03.main("../input/day03/input")
+    day04.main("../input/day04/input.txt")
+    day05.main("../input/day05/input.txt")
+    day06.main(["../input/day06/part1", "../input/day06/part2"])
 
 
 if __name__ == "__main__":
