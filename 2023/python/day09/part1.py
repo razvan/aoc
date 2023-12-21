40fc4c3de82f385b4a571a2814c812e9605d2aac
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-12-21 11:56:21 +0100

    2023 day09 part 1

--- 2023/python/day09/part1.py
+++ 2023/python/day09/part1.py
@@ -1,26 +1,47 @@
 import re
 from dataclasses import dataclass
 from typing import List
-
-from parsy import generate, regex
+from itertools import pairwise
+from parsy import eof, generate, regex, string, success
 
 
 @dataclass(frozen=True, slots=True)
 class Puzzle:
-    pass
+    history: List[List[int]]
 
 
 def run(f: str):
     with open(f) as input:
         puzzle = parser.parse(input.read())
-        print("Day xx: part 1 is {}".format(solve(puzzle)))
+        print("Day 09: part 1 is {}".format(solve(puzzle)))
 
 
 def solve(puzzle: Puzzle) -> int:
-    return 1
+    return sum([predict(h) for h in puzzle.history])
+
+
+def predict(history: List[int]) -> int:
+    diffs: List[List[int]] = [history]
+
+    while any(diffs[-1]):
+        tmp = list(map(lambda t: t[1] - t[0], pairwise(diffs[-1])))
+        diffs.append(tmp)
+
+    return sum([vals[-1] for vals in reversed(diffs)])
 
 
 @generate
 def parser():
-    yield regex(".*", re.DOTALL)
-    return Puzzle()
+    number = regex(r"-?\d+").map(int).desc("number")
+    padding = regex(r"\s*").desc("padding")
+    newline = regex(r"\n").desc("new line")
+    singlespace = string(" ")
+
+    history = []
+    while True:
+        vals = yield padding >> number.sep_by(singlespace) << newline | success(None)
+        if not vals:
+            break
+        history.append(vals)
+        yield padding | eof
+    return Puzzle(history)
--- 2023/python/day09/test_part1.py
+++ 2023/python/day09/test_part1.py
@@ -1,8 +1,28 @@
 from unittest import TestCase
 
-from .part1 import Puzzle, parser, solve
+from .part1 import Puzzle, parser, solve, predict
 
 
 class Part1(TestCase):
-    def test_dayxx_(self):
-        self.assertTrue(True)
+    def test_day09_parse(self):
+        input = """
+            0 3 6 9 12 15
+            1 3 6 10 15 21
+            10 13 16 21 30 45
+        """
+        expected = Puzzle(
+            [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
+        )
+        got = parser.parse(input)
+        self.assertEqual(expected, got)
+
+    def test_day09_solve(self):
+        p = Puzzle(
+            [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
+        )
+
+        self.assertEqual(114, solve(p))
+
+    def test_day09_predict(self):
+        input = [10, 13, 16, 21, 30, 45]
+        self.assertEqual(68, predict(input))
