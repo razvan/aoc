d34aa4f83969b3315aaf8ad129e8fb00e132ce51
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-12-21 12:33:27 +0100

    2023 day09 part 2

--- 2023/python/day09/part2.py
+++ 2023/python/day09/part2.py
@@ -1,26 +1,43 @@
-import re
 from dataclasses import dataclass
 from typing import List
-
-from parsy import generate, regex
+from itertools import pairwise
+from parsy import eof, generate, regex, string, success
 
 
 @dataclass(frozen=True, slots=True)
 class Puzzle:
-    pass
+    history: List[List[int]]
 
 
 def run(f: str):
     with open(f) as input:
         puzzle = parser.parse(input.read())
-        print("Day xx: part 2 is {}".format(solve(puzzle)))
+        print("Day 09: part 2 is {}".format(solve(puzzle)))
 
 
 def solve(puzzle: Puzzle) -> int:
-    return 2
+    return sum([predict(h) for h in puzzle.history])
+
+
+def predict(history: List[int]) -> int:
+    if any(history):
+        return history[0] - predict(list(map(lambda t: t[1] - t[0], pairwise(history))))
+    else:
+        return 0
 
 
 @generate
 def parser():
-    yield regex(".*", re.DOTALL)
-    return Puzzle()
+    number = regex(r"-?\d+").map(int).desc("number")
+    padding = regex(r"\s*").desc("padding")
+    newline = regex(r"\n").desc("new line")
+    singlespace = string(" ")
+
+    history = []
+    while True:
+        vals = yield padding >> number.sep_by(singlespace) << newline | success(None)
+        if not vals:
+            break
+        history.append(vals)
+        yield padding | eof
+    return Puzzle(history)
--- 2023/python/day09/test_part2.py
+++ 2023/python/day09/test_part2.py
@@ -1,8 +1,19 @@
 from unittest import TestCase
 
-from .part2 import Puzzle, parser, solve
+from .part2 import Puzzle, solve, predict
 
 
 class Part2(TestCase):
-    def test_dayxx_(self):
-        self.assertTrue(True)
+    def test_day09_part2_predict(self):
+        # self.assertEqual(5, predict([10, 13, 16, 21, 30, 45]))
+        self.assertEqual(0, predict([1, 3, 6, 10, 15, 21]))
+        # 0 1 3 6 10 15 21
+        # 1 2 3 4 5 6
+        # 1 1 1 1 1
+        # 0 0 0 0
+
+    def test_day09_part2_solve(self):
+        p = Puzzle(
+            [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
+        )
+        self.assertEqual(2, solve(p))  # -3, 0, 5
