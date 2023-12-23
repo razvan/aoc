6af0ea94f5d481cf677e747a5701e31696be09da
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-12-23 21:13:47 +0100

    2023 day10 part two failed

--- 2023/python/day10/part2.py
+++ 2023/python/day10/part2.py
@@ -1,25 +1,192 @@
-import re
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from heapq import heapify
+from itertools import pairwise, takewhile
+from typing import (
+    Dict,
+    List,
+    Set,
+    Optional,
+    Generator,
+)
 
-from parsy import generate, regex
+from parsy import generate, regex, success, eof, string
+
+
+class Day10Error(Exception):
+    pass
+
+
+TILE_CONN: Dict[str, List[str]] = {
+    "|": ["N", "S"],
+    "-": ["E", "W"],
+    "L": ["N", "E"],
+    "J": ["N", "W"],
+    "7": ["S", "W"],
+    "F": ["S", "E"],
+    ".": [],
+    "S": ["N", "S", "W", "E"],
+}
+
+
+@dataclass(frozen=True, slots=True, order=True)
+class Loc:
+    x: int
+    y: int
+    tile: str = field(default=".", compare=False)
 
 
 @dataclass(frozen=True, slots=True)
 class Puzzle:
-    pass
+    tiles: List[str]
+    start: Loc
+    width: int
+    height: int
+
+    def __iter__(self) -> Generator[Loc, None, None]:
+        for i, row in enumerate(self.tiles):
+            for j, tile in enumerate(row):
+                yield Loc(i, j, tile)
+
+
+def peek_tile(
+    dir: str, curr: Loc, p: Puzzle, seen: Set[Loc], prev: Optional[Loc] = None
+) -> Optional[Loc]:
+    match dir:
+        case "N":
+            # check north
+            if curr.x - 1 >= 0:
+                tile = p.tiles[curr.x - 1][curr.y]
+                north = Loc(curr.x - 1, curr.y, tile)
+                if north not in seen and north != prev:
+                    if "S" in TILE_CONN[tile]:
+                        seen.add(north)
+                        return north
+        case "S":
+            # check south
+            if curr.x + 1 < p.height:
+                tile = p.tiles[curr.x + 1][curr.y]
+                south = Loc(curr.x + 1, curr.y, tile)
+                if south not in seen and south != prev:
+                    if "N" in TILE_CONN[tile]:
+                        seen.add(south)
+                        return south
+        case "W":
+            # check west
+            if curr.y - 1 >= 0:
+                tile = p.tiles[curr.x][curr.y - 1]
+                west = Loc(curr.x, curr.y - 1, tile)
+                if west not in seen and west != prev:
+                    if "E" in TILE_CONN[tile]:
+                        seen.add(west)
+                        return west
+        case "E":
+            # check east
+            if curr.y + 1 < p.width:
+                tile = p.tiles[curr.x][curr.y + 1]
+                east = Loc(curr.x, curr.y + 1, tile)
+                if east not in seen and east != prev:
+                    if "W" in TILE_CONN[tile]:
+                        seen.add(east)
+                        return east
+    return None
+
+
+def next_tile(
+    curr: Loc, p: Puzzle, seen: Set[Loc], prev: Optional[Loc] = None
+) -> Optional[Loc]:
+    """Find the next tile has not been [seen] yet and is not the [prev] tile
+    (the one before the [curr] tile).
+    The check for previous is needed to avoid a tile in the immediate
+    vicinity of the [start] location returning [start] as the next tile at
+    the beginning of the search.
+    This can happen because the [start] location is never added to the [seen]
+    set."""
+    for dir in TILE_CONN[curr.tile]:
+        if loc := peek_tile(dir, curr, p, seen, prev):
+            return loc
+    return None
+
+
+def find_loop(p: Puzzle) -> List[Loc]:
+    res: List[Loc] = [p.start]
+    seen: Set[Loc] = set()
+    while lres := len(res):
+        prev: Optional[Loc] = res[-2] if lres > 1 else None
+        next = next_tile(res[-1], p, seen, prev)
+        if next == p.start:
+            # found the loop
+            return res
+        elif next is None:
+            # backtrack
+            res.pop()
+        else:
+            res.append(next)
+
+    raise Day10Error("Failed to find a loop starting at {}".format(p.start))
+
+
+def find_poly_area(poly: List[Loc], puzzle: Puzzle) -> int:
+    # spoly = sorted(poly)
+
+    # print(f"spoly={spoly}")
+
+    def _is_in(loc: Loc) -> bool:
+        res = 0
+        for p in poly:
+            # print(f"----\ncomparing loc={loc} with p={p}")
+            if p.x != loc.x:
+                # print("Skip. Not on the same line")
+                continue
+            if p.y >= loc.y:
+                # print("Skip. loc is before p")
+                continue
+            if loc in poly:
+                # print("Skip. loc is on poly")
+                continue
+            if p.tile == "-" or p.tile == "J" or p.tile == "L":
+                # print("Skip. p is in [-JL]")
+                continue
+            # print("Found. p is a bar before loc")
+            res += 1
+        return res % 2 == 1
+
+    _in = list(filter(_is_in, iter(puzzle)))
+    # print(_in)
+    return len(_in)
 
 
 def run(f: str):
     with open(f) as input:
         puzzle = parser.parse(input.read())
-        print("Day xx: part 2 is {}".format(solve(puzzle)))
+        # This takes very long to run so it's basically not solved.
+        # The tests pass but I doun't know if the actual result would be correct
+        # because I don't understand why disregarding [-JL] when scanning the tiles
+        # gives the correct result.
+        print("Day 10: part 2 is NOT SOLVED!")  # .format(solve(puzzle)))
 
 
 def solve(puzzle: Puzzle) -> int:
-    return 2
+    return find_poly_area(find_loop(puzzle), puzzle)
 
 
 @generate
 def parser():
-    yield regex(".*", re.DOTALL)
-    return Puzzle()
+    pipe = regex(r"[.SFJ7L|-]+")
+    newline = string("\n")
+    padding = regex(r"\s*")
+    tiles: List[str] = []
+    start = None
+    while True:
+        line = yield (padding >> pipe << newline) | success(None)
+        if not line:
+            break
+        if not start:
+            try:
+                start = Loc(len(tiles), line.index("S"), "S")
+            except ValueError:
+                pass
+        tiles.append(line)
+        yield padding | eof
+    if not start:
+        raise Day10Error("Couldn't find the start location")
+    return Puzzle(tiles, start, len(tiles[0]), len(tiles))
--- 2023/python/day10/test_part2.py
+++ 2023/python/day10/test_part2.py
@@ -1,8 +1,163 @@
 from unittest import TestCase
 
-from .part2 import Puzzle, parser, solve
+from .part2 import Puzzle, parser, solve, Loc, find_loop
 
 
 class Part2(TestCase):
-    def test_dayxx_(self):
-        self.assertTrue(True)
+    def test_day10_part2_parse(self):
+        input = """
+        .F----7F7F7F7F-7....
+        .|F--7||||||||FJ....
+        .||.FJ||||||||L7....
+        FJL7L7LJLJ||LJ.L-7..
+        L--J.L7...LJS7F-7L7.
+        ....F-J..F7FJ|L7L7L7
+        ....L7.F7||L7|.L7L7|
+        .....|FJLJ|FJ|F7|.LJ
+        ....FJL-7.||.||||...
+        ....L---J.LJ.LJLJ...
+        """
+        expected = Puzzle(
+            tiles=[
+                ".F----7F7F7F7F-7....",
+                ".|F--7||||||||FJ....",
+                ".||.FJ||||||||L7....",
+                "FJL7L7LJLJ||LJ.L-7..",
+                "L--J.L7...LJS7F-7L7.",
+                "....F-J..F7FJ|L7L7L7",
+                "....L7.F7||L7|.L7L7|",
+                ".....|FJLJ|FJ|F7|.LJ",
+                "....FJL-7.||.||||...",
+                "....L---J.LJ.LJLJ...",
+            ],
+            start=Loc(4, 12, "S"),
+            width=20,
+            height=10,
+        )
+        self.assertEqual(expected, parser.parse(input))
+
+    def test_day10_part2_iter(self):
+        puzzle = Puzzle(
+            tiles=[
+                ".F----7F7F7F7F-7....",
+            ],
+            start=Loc(4, 12, "S"),
+            width=20,
+            height=10,
+        )
+        it = iter(puzzle)
+        self.assertEqual(Loc(0, 0, "."), next(it))
+        self.assertEqual(Loc(0, 1, "F"), next(it))
+        self.assertEqual(Loc(0, 2, "-"), next(it))
+
+    def test_day10_part2_solve(self):
+        puzzle = Puzzle(
+            tiles=[
+                ".F----7F7F7F7F-7....",
+                ".|F--7||||||||FJ....",
+                ".||.FJ||||||||L7....",
+                "FJL7L7LJLJ||LJ.L-7..",
+                "L--J.L7...LJS7F-7L7.",
+                "....F-J..F7FJ|L7L7L7",
+                "....L7.F7||L7|.L7L7|",
+                ".....|FJLJ|FJ|F7|.LJ",
+                "....FJL-7.||.||||...",
+                "....L---J.LJ.LJLJ...",
+            ],
+            start=Loc(4, 12, "S"),
+            width=20,
+            height=10,
+        )
+        self.assertEqual(8, solve(puzzle))
+
+    def test_day10_part2_solve2(self):
+        puzzle = Puzzle(
+            tiles=[
+                "FF7FSF7F7F7F7F7F---7",
+                "L|LJ||||||||||||F--J",
+                "FL-7LJLJ||||||LJL-77",
+                "F--JF--7||LJLJ7F7FJ-",
+                "L---JF-JLJ.||-FJLJJ7",
+                "|F|F-JF---7F7-L7L|7|",
+                "|FFJF7L7F-JF7|JL---7",
+                "7-L-JL7||F7|L7F-7F7|",
+                "L.L7LFJ|||||FJL7||LJ",
+                "L7JLJL-JLJLJL--JLJ.L",
+            ],
+            start=Loc(0, 4, "S"),
+            width=20,
+            height=10,
+        )
+        self.assertEqual(10, solve(puzzle))
+
+    def test_day10_part2_solve3(self):
+        puzzle = Puzzle(
+            tiles=["..F7.", ".FJ|.", "SJ.L7", "|F--J", "LJ..."],
+            start=Loc(2, 0, "S"),
+            width=5,
+            height=5,
+        )
+        self.assertEqual(1, solve(puzzle))
+
+    def test_day10_part2_find_loop(self):
+        p = Puzzle(
+            tiles=["..F7.", ".FJ|.", "SJ.L7", "|F--J", "LJ..."],
+            start=Loc(2, 0, "S"),
+            width=5,
+            height=5,
+        )
+        expected = [
+            Loc(x=2, y=0),
+            Loc(x=3, y=0),
+            Loc(x=4, y=0),
+            Loc(x=4, y=1),
+            Loc(x=3, y=1),
+            Loc(x=3, y=2),
+            Loc(x=3, y=3),
+            Loc(x=3, y=4),
+            Loc(x=2, y=4),
+            Loc(x=2, y=3),
+            Loc(x=1, y=3),
+            Loc(x=0, y=3),
+            Loc(x=0, y=2),
+            Loc(x=1, y=2),
+            Loc(x=1, y=1),
+            Loc(x=2, y=1),
+        ]
+        self.assertEqual(expected, find_loop(p))
+
+    def test_day10_part2_sorted(self):
+        path = [
+            Loc(x=4, y=0),
+            Loc(x=4, y=1),
+            Loc(x=2, y=0),
+            Loc(x=3, y=0),
+        ]
+        expected = [
+            Loc(x=2, y=0),
+            Loc(x=3, y=0),
+            Loc(x=4, y=0),
+            Loc(x=4, y=1),
+        ]
+        self.assertEqual(expected, sorted(path))
+
+    def test_day10_part2_intersect(self):
+        s1 = [
+            Loc(x=4, y=2, tile="|"),
+            Loc(x=4, y=1, tile="|"),
+            Loc(x=2, y=0, tile="|"),
+            Loc(x=3, y=2, tile="|"),
+        ]
+        s2 = [
+            Loc(x=4, y=0, tile="-"),
+            Loc(x=4, y=1, tile="-"),
+            Loc(x=2, y=0, tile="-"),
+            Loc(x=3, y=0, tile="-"),
+        ]
+        expected = set(
+            [
+                Loc(x=4, y=1, tile="|"),
+                Loc(x=2, y=0, tile="-"),
+            ]
+        )
+        self.assertEqual(expected, set(s1).intersection(s2))
